4b88ccd023ef842763c2fee4c27bb17f7d7a5a95
Preisler <preisler.andras@gmail.com>
2023-06-30 08:34:11 +0200

    mian

---
+++ buildingAssets/pages/infoPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function InfoPage() {
+    return (
+        <View>
+            <Text>InfoPage</Text>
+        </View>
+    );
+}
+
+export default InfoPage;
---
+++ buildingAssets/pages/postPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function PostPage() {
+    return (
+        <View>
+            <Text>mainPage</Text>
+        </View>
+    );
+}
+
+export default PostPage;
