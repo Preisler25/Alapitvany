c597e101fdb3eb3728a2c15cb3c944811171714d
prezli <preisler.andras@gmail.com>
2023-07-01 21:40:28 +0200

    main

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native'; // https://reactnavigation.org/docs/getting-started
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import PostPage from './buildingAssets/pages/postPage';
